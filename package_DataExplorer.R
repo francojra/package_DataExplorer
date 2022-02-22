cf4651573ab927393aa2e8bee20291a3fe2a53bb
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-02-21 23:57:23 -0300

    Add files via upload

---
+++ package_DataExplorer.R
@@ -0,0 +1,16 @@
+
+# Pacote DataExplorer ----------------------------------------------------------------------------------------------------------------------------
+# Referência: R Descomplicado --------------------------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 21/02/22 ---------------------------------------------------------------------------------------------------------------------------
+
+
+# Sobre o pacote ---------------------------------------------------------------------------------------------------------------------------
+
+## O DataExplorer simplifica e automatiza as análises exploratórias de dados. O
+## pacote automaticamente escaneia a performance e o perfil de cada variável. Ele
+## Também oferece várias funções que geram diferentes gráficos de variáveis 
+## discretas ou contínuas.
+
+## De forma interessante, p pacote também gera um documento HTML com a ajuda
+## da função create_report.
