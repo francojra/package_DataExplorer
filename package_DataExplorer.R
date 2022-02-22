8fb4dd977dd059299b3fff7c856b6ad0726fae99
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-02-22 00:17:13 -0300

    Add files via upload

--- package_DataExplorer.R
+++ package_DataExplorer.R
@@ -14,3 +14,25 @@
 
 ## De forma interessante, p pacote também gera um documento HTML com a ajuda
 ## da função create_report.
+
+# Carregar dados e observar primeiras linhas -----------------------------------------------------------------------------------------------
+
+# install.packages("tidyverse")
+library(dplyr)
+library(readr)
+
+# read csv file
+df <- read.csv("C:\\Users\\jeann\\OneDrive\\Área de Trabalho\\cardio_train.csv", 
+               sep = ";")
+df
+
+# pre-processing
+df <- 
+  # remove the id
+  select(df, -id) %>%
+  # age: days -> years
+  mutate(age = round(age / 365))
+
+
+# observe first rows
+head(df)
